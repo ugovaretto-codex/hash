bde428ab281bd02202fdff2f2b61b1c86e2e79bd
Ugo Varetto <ugovaretto@gmail.com>
2020-05-28 01:28:38 +0800

    added hmac for bytes

--- hmac.h
+++ hmac.h
@@ -29,6 +29,7 @@
 
 #include <string>
 #include <cstring> // memcpy
+#include <vector>
 
 /// compute HMAC hash of data and key using MD5, SHA1 or SHA256
 template <typename HashMethod>
@@ -74,7 +75,55 @@ std::string hmac(const void* data, size_t numDataBytes, const void* key, size_t 
   return finalHasher.getHash();
 }
 
+/// compute HMAC hash of data and key using MD5, SHA1 or SHA256
+template <typename HashMethod>
+std::vector<uint8_t> hmacb(const std::vector<uint8_t>& d, 
+                           const std::vector<uint8_t>& k)
+{
+  const void* data = d.data();
+  const size_t numDataBytes = d.size();
+  const void* key = k.data();
+  const size_t numKeyBytes = k.size();
+  // initialize key with zeros
+  unsigned char usedKey[HashMethod::BlockSize] = {0};
 
+  // adjust length of key: must contain exactly blockSize bytes
+  if (numKeyBytes <= HashMethod::BlockSize)
+  {
+    // copy key
+    memcpy(usedKey, key, numKeyBytes);
+  }
+  else
+  {
+    // shorten key: usedKey = hashed(key)
+    HashMethod keyHasher;
+    keyHasher.add(key, numKeyBytes);
+    keyHasher.getHash(usedKey);
+  }
+
+  // create initial XOR padding
+  for (size_t i = 0; i < HashMethod::BlockSize; i++)
+    usedKey[i] ^= 0x36;
+
+  // inside = hash((usedKey ^ 0x36) + data)
+  unsigned char inside[HashMethod::HashBytes];
+  HashMethod insideHasher;
+  insideHasher.add(usedKey, HashMethod::BlockSize);
+  insideHasher.add(data,    numDataBytes);
+  insideHasher.getHash(inside);
+
+  // undo usedKey's previous 0x36 XORing and apply a XOR by 0x5C
+  for (size_t i = 0; i < HashMethod::BlockSize; i++)
+    usedKey[i] ^= 0x5C ^ 0x36;
+
+  // hash((usedKey ^ 0x5C) + hash((usedKey ^ 0x36) + data))
+  HashMethod finalHasher;
+  finalHasher.add(usedKey, HashMethod::BlockSize);
+  finalHasher.add(inside,  HashMethod::HashBytes);
+  std::vector<uint8_t> b(HashMethod::HashBytes);
+  finalHasher.getHash(b.data());
+  return b;
+}
 /// convenience function for std::string
 template <typename HashMethod>
 std::string hmac(const std::string& data, const std::string& key)
